75c337db74e5dea04644d753fe6a3b94c0e1f3b8
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-04-12 00:37:25 +0530

    mod 7 init

--- Module-05/n.cpp
+++ Module-05/n.cpp
@@ -18,3 +18,24 @@ int main()
 
     return 0;
 }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int year;
+//     cout << "Enter the year: ";
+//     cin >> year;
+
+//     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
+//     {
+//         cout << "Leap year" << endl;
+//     }
+//     else
+//     {
+//         cout << "Not a leap year" << endl;
+//     }
+
+//     return 0;
+// }
