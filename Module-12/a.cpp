2c1bf2974a21d119bd9b7351a9496f8213b76fe4
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-04-13 20:13:56 +0530

    module 12 init

---
+++ Module-12/a.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void printSubarrays(int *arr, int n)
+{
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            cout << "(" << start << "," << end << ") "; // start and end
+
+            for (int i = start; i <= end; i++)
+            {
+                cout << arr[i]; // actual elements
+            }
+            cout << ", ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(int);
+
+    printSubarrays(arr, n);
+    return 0;
+}
--- Module-12/notes.md
+++ Module-12/notes.md
@@ -2,7 +2,7 @@
 
 Advanced Concepts and Questions
 
-PRINT SUBARRAY
+PRINT SUBARRAY -> a.cpp
 Subarray is continuous part of an array
 
 1,2,3,4,5 -> original array
@@ -14,3 +14,12 @@ Subarray is continuous part of an array
 5
 
 Subsequence is non-continuous but they must just follow an order
+
+same questions can be asked for strings, vectors
+
+for a n sized array teh sub arrays are = n + n-1 + n-2 = n-3 . . . 1 = n(n+1)/2
+
+time complexity -> O(n^3) -> as three loops are used
+this can be optimised using strings to O(n^2);
+
+space complexity -> O(1) -> no extra space is used
