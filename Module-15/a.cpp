50b50624dff2b21bee8b2c2f7442149ab808652d
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-04-17 23:19:25 +0530

    char and strings

--- Module-15/a.cpp
+++ Module-15/a.cpp
@@ -3,6 +3,19 @@ using namespace std;
 
 int main()
 {
+    char ch1 = 'a';
+    char ch2 = 'A';
+    char ch3 = '1';
+    char ch4 = '!';
+    char ch5 = '\n'; // fixed
+
+    cout << ch1 << endl;
+    cout << int(ch1) << endl;
+
+    // to get the position of some char in alph order
+    char ch6 = 'f';
+    int pos = ch6 - 'a';
+    cout << pos << endl;
 
     return 0;
 }
--- Module-15/notes.md
+++ Module-15/notes.md
@@ -1,1 +1,12 @@
 # DSA with C++ — Module 15 Notes
+
+CHAR DATA TYPE
+
+char -> char array[] -> strings
+
+chat data type -> stores character -> 1 byte memory
+a.cpp
+we can store lower case elements, upper case elements, special characters, single digits
+we can also store /n, /t
+
+all the chars are encoded as ASCII -> American Standard COde for Information Interchange
