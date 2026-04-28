4cf7635676504feb239ef730840c846693c6f50b
Rajat Saraswat <rajat.saraswat.0409@gmail.com>
2026-04-28 19:57:45 +0530

    object created

---
+++ Module-18/b.cpp
@@ -0,0 +1,28 @@
+#incldue <isotream>
+using namespace std;
+
+
+class User {
+    // properties
+    string name;
+    int age;
+    string email;
+    string password;
+
+    // methods
+    void login(){
+        cout << "Login successful" << endl;
+    }
+}
+
+int main(){
+
+    User u1; // object is created
+    u1.name = "John";
+    u1.age = 20;
+    u1.email = "john@example.com";
+    u1.password = "123456";
+    u1.login();
+
+    return 0;
+}
